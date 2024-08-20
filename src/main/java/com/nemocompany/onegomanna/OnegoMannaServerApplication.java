90453863c819208cf7d4f4a6afae803ac838f03f
moveuk <v_donguk@naver.com>
2024-08-20 14:50:55 +0900

    config: Springboot 프로젝트 생

---
+++ src/main/java/com/nemocompany/onegomanna/OnegoMannaServerApplication.java
@@ -0,0 +1,13 @@
+package com.nemocompany.onegomanna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnegoMannaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnegoMannaServerApplication.class, args);
+    }
+
+}
