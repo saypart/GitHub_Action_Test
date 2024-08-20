667aae5c79cc3908b5ce7249eec1e63bd6c53906
moveuk <v_donguk@naver.com>
2024-08-20 17:45:22 +0900

    feat: 서버 헬스 체크 api 명세 추가

--- src/main/java/com/nemocompany/onegomanna/api/HealthCheckController.java
+++ src/main/java/com/nemocompany/onegomanna/api/HealthCheckController.java
@@ -1,10 +1,18 @@
 package com.nemocompany.onegomanna.api;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthCheckController {
+
+    @Operation(summary = "health check", description = "health check API")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "서버 정상 가동")
+    })
     @GetMapping("/ping")
     public String healthCheck() {
         return "pong";
